eafd745fdaced687b087b3f790463b9efb274dc9
victoriahuynh <victoriroly17@gmail.com>
2018-11-27 14:12:54 -0800

    starting ui.R file

---
+++ ui.R
@@ -0,0 +1,8 @@
+library(shiny)
+
+#fill in box: "what drugs are you prescribed"
+
+#dropdown menu: "how are you feeling?", with emoticons to select
+#next dropdown menu: "which one do you want to see?"
+
+#visualization: tabs of each drug, line graph tracking moods

7321c8c3fb07caf8ee6ad4821f54eecd011061db
Veronica Chang <vchang9@uw.edu>
2018-11-30 11:20:18 -0800

    Fix widgets

--- App/ui.R
+++ App/ui.R
@@ -1,36 +1,35 @@
-#fill in box: "what drugs are you prescribed"
+library(ggvis)
 
-#dropdown menu: "how are you feeling?", with emoticons to select
-#next dropdown menu: "which one do you want to see?"
+# For dropdown menu
+actionLink <- function(inputId, ...) {
+  tags$a(href='javascript:void',
+         id=inputId,
+         class='action-button',
+         ...)
+}
 
-#visualization: tabs of each drug, line graph tracking moods
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
+fluidPage(
   titlePanel("Mood Tracker"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-      selectInput("How are you feeling?", label = h3("Choose a feeling"), 
-                  choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
-                  selected = 1),
-    
-      hr(),
-     fluidRow(column(3, verbatimTextOutput("value")))
-    # Show a plot of the generated distribution
-    # mainPanel(
-    #    plotOutput("distPlot")
-    # )
+  fluidRow(
+    column(3,
+           wellPanel(
+             h4("Filter"),
+             textInput("drug", "What drugs are you currently prescribed?"),
+             selectInput("How are you feeling?", "Choose your feeling",
+                         c("Happy" = 1, "Neutral" = 2, "Sad" = 3), selected = 1)
+             )
+           ),
+           wellPanel(
+             selectInput("xvar", "X-axis variable", axis_vars, selected = "Meter"),
+             selectInput("yvar", "Y-axis variable", axis_vars, selected = "Reviews")
+             ))
+           )
+    column(9,
+           ggvisOutput("plot1"),
+           wellPanel(
+             span("Number of movies selected:",
+                  textOutput("n_movies")
+             )
+           )
+    )
   )
-))

64210f7660a67cc917828b6dff4c4c868a5f872a
Veronica Chang <vchang9@uw.edu>
2018-11-30 10:52:52 -0800

    Made comment changes

--- App/ui.R
+++ App/ui.R
@@ -1,8 +1,31 @@
-library(shiny)
-
 #fill in box: "what drugs are you prescribed"
 
 #dropdown menu: "how are you feeling?", with emoticons to select
 #next dropdown menu: "which one do you want to see?"
 
 #visualization: tabs of each drug, line graph tracking moods
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Mood Tracker"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("bins",
+                   "Number of bins:",
+                   min = 1,
+                   max = 50,
+                   value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))

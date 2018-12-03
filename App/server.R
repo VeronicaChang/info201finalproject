fd70a3717ccbb2101b3466581a8ac6ff90a12213
Veronica Chang <vchang9@uw.edu>
2018-12-03 14:50:13 -0800

    Start again

--- App/server.R
+++ App/server.R
@@ -1,99 +1,43 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(ggvis)
+library(ggplot2)
 library(dplyr)
-drug_names <- read.table(file = "../data/drug_names.tsv", header = T, stringsAsFactors = F, sep = "/")
-side_effects <- read.delim("../data/meddra.tsv", stringsAsFactors = F, sep = "/")
-all_indications <- read.delim("../data/meddra_all_indications.tsv", stringsAsFactors = F, sep = "/")
-
-  #Sorting and renaming column names to make it more readable
-  colnames(side_effects) <- c("UMLS ID", "MedDRA ID", "kind", "side effect")
-  
-  names_with_id <- drug_names %>% 
-    right_join(all_indications) %>% 
-    select(carnitine, C0015544) %>% 
-    na.omit()
-  colnames(names_with_id) <- c("Drug_Name", "UMLS ID")
-  
-  #Joining the data
-  final_table <- right_join(names_with_id, side_effects)
-  final_table <- na.omit(final_table)
 
 
-if (FALSE) {
-  library(RSQLite)
-  library(dbplyr)
-}
 
-# Set up handles to database tables on app start
-#source("final_data.R")
-data <- give_data()
+drug_names <- read.delim(file = "~/Desktop/INFO201/info201finalproject/data/drug_names.tsv", header = T)
+side_effects <- read.delim("~/Desktop/INFO201/info201finalproject/data/meddra.tsv", stringsAsFactors = F)
+all_indications <- read.delim("~/Desktop/INFO201/info201finalproject/data/meddra_all_indications.tsv", stringsAsFactors = F)
 
+#Sorting and renaming column names to make it more readable
+colnames(all_indications) <- c("UMLS_ID", "MedDRA_ID", "kind", "first_effect", "type", "number", "second_effect")
+colnames(drug_names) <- c("UMLS_ID", "drug")
+colnames(side_effects) <- c("MedDRA_ID", "type", "number", "side_effect")
+
+names_with_id <- right_join(side_effects, all_indications, by = "MedDRA_ID") %>% select("UMLS_ID", "side_effect")
+drug_names$UMLS_ID <- as.character(drug_names$UMLS_ID)
+
+#Joining the data
+final_table <- left_join(drug_names, names_with_id, by = "UMLS_ID") %>% select("drug", "side_effect") 
+final_table <- na.omit(final_table) %>% select(drug, side_effect)
 function(input, output, session) {
-  
-  # Filter the movies, returning a data frame
-  movies <- reactive({
-    # Apply filters
-    data <- data %>% filter(Drug_Name == input$drug) %>% select(Drug_Name, side_effect)
-    # Optional: filter by director
-    if (!is.null(input$drug) && input$director != "") {
-      drug <- paste0("%", input$director, "%")
-      data <- data %>% filter(Drug_Name %like% drug)
-    }
-    data <- as.data.frame(data)
-  })
-  
-  # Function for generating tooltip text
-  movie_tooltip <- function(x) {
-    if (is.null(x)) return(NULL)
-    if (is.null(x$ID)) return(NULL)
-    
-    # Pick out the movie with this ID
-    all_movies <- isolate(movies())
-    movie <- all_movies[all_movies$ID == x$ID, ]
-    
-    paste0("<b>", movie$Title, "</b><br>",
-           movie$Year, "<br>",
-           "$", format(movie$BoxOffice, big.mark = ",", scientific = FALSE)
-    )
+  # Filter by the drug the user is prescribed
+  cat("ono")
+  if (!is.null(input$drug_name)) {
+    data <- data %>% filter(drug == input$drug_name) 
   }
-  
-  # A reactive expression with the ggvis plot
-  vis <- reactive({
-    # Lables for axes
-    #xvar_name <- names(axis_vars)[axis_vars == input$xvar]
-    #yvar_name <- names(axis_vars)[axis_vars == input$yvar]
+  my_data <- as.data.frame(data)
+  cat("printingaiya")
+  output$text <- renderText({
+    print(input$drug_name)
+  })
+  cat("plottyyeryeo")
+  output$plot2 <- renderPlot({
+    # Create the main ggplot
+    p <- ggplot(data = my_data, aes(x = 'drug', y = 'side_effect'))+geom_line()
     
-    # Normally we could do something like props(x = ~BoxOffice, y = ~Reviews),
-    # but since the inputs are strings, we need to do a little more work.
-    xvar <- prop("x", as.symbol(input$xvar))
-    yvar <- prop("y", as.symbol(input$yvar))
-    
-    movies %>%
-      ggvis(x = xvar, y = yvar) %>%
-      layer_points(size := 50, size.hover := 200,
-                   fillOpacity := 0.2, fillOpacity.hover := 0.5,
-                   stroke = ~has_oscar, key := ~ID) %>%
-      add_tooltip(movie_tooltip, "hover") %>%
-      add_axis("x", title = xvar_name) %>%
-      add_axis("y", title = yvar_name) %>%
-      add_legend("stroke", title = "Won Oscar", values = c("Yes", "No")) %>%
-      scale_nominal("stroke", domain = c("Yes", "No"),
-                    range = c("orange", "#aaa")) %>%
-      set_options(width = 500, height = 500)
+    return(p)
   })
   
-  vis %>% bind_shiny("plot1")
-  
-  output$n_movies <- renderText({ nrow(movies()) })
 }
 
 #y <-data.frame(matrix(ncol = 1, nrow = 3))
--- App/ui.R
+++ App/ui.R
@@ -1,36 +1,16 @@
-## install.packages("ggvis")
-library(ggvis)
-#source("final_data.R")
-data <- give_data()
-# For dropdown menu
-actionLink <- function(inputId, ...) {
-  tags$a(href='javascript:void',
-         id=inputId,
-         class='action-button',
-         ...)
-}
+library(shiny)
+library(ggplot2)
 
-fluidPage(
-  titlePanel("Mood Tracker"),
-  fluidRow(
-    column(3,
-           wellPanel(
-             h4("Filter"),
-             textInput("drug", "What drugs are you currently prescribed?"),
-             selectInput("How are you feeling?", "Choose your feeling",
-                         c("Happy" = 1, "Neutral" = 2, "Sad" = 3), selected = 1),
-             selectInput("xvar", "X-axis variable", colnames(data), selected = "Meter"),
-             selectInput("yvar", "Y-axis variable", colnames(data), selected = "Reviews")
-             )
-           ),
-    column(9,
-           ggvisOutput("plot1"),
-           wellPanel(
-             span("Number of movies selected:",
-                  textOutput("n_movies")
-             )
-           )
+shinyUI(fluidPage(
+  fluidPage(
+    titlePanel("Mood Tracker"),
+    sidebarLayout(
+      sidebarPanel(
+        textInput('drug_name', 'Prescribed Drug Name', value="gamma-aminobutyric")
+      ),
+      mainPanel(
+        plotOutput("plot2"))
     )
   )
 )
-
+)

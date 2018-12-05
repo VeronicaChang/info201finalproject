bbb6c94234b679802aad654deb5143aa47b5d00a
victoriahuynh <victoriroly17@gmail.com>
2018-12-04 21:09:11 -0800

    updated with new tabs: maps, side effects

--- App/server.R
+++ App/server.R
@@ -2,14 +2,28 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(lubridate)
-
+#install.packages("leaflet)
+library(leaflet)
+#install.packages(plotly)
+library(plotly)
+#install.packages(Hmisc)
+library(Hmisc)
 
 shinyServer(function(input, output) {
+  
+  output$map <- renderLeaflet({
+    health <- read.csv("~/Desktop/INFO201/info201finalproject/data/map_data.csv", stringsAsFactors = F)
+    m <- leaflet(health) %>%
+      addTiles() %>%
+      addMarkers(~lng, ~lat, label = health$Clinic.name)
+    return(m)
+  })
+  
   dataInput <- reactive({
     # Filter by the drug the user is prescribed
-    drug_names <- read.delim(file = "~/INFO201/info201finalproject/data/drug_names.tsv", header = T)
-    side_effects <- read.delim("~/INFO201/info201finalproject/data/meddra.tsv", stringsAsFactors = F)
-    all_indications <- read.delim("~/INFO201/info201finalproject/data/meddra_all_indications.tsv", stringsAsFactors = F)
+    drug_names <- read.delim(file = "~/Desktop/INFO201/info201finalproject/data/drug_names.tsv", header = T)
+    side_effects <- read.delim("~/Desktop/INFO201/info201finalproject/data/meddra.tsv", stringsAsFactors = F)
+    all_indications <- read.delim("~/Desktop/INFO201/info201finalproject/data/meddra_all_indications.tsv", stringsAsFactors = F)
     
     #Sorting and renaming column names to make it more readable
     colnames(all_indications) <- c("UMLS_ID", "MedDRA_ID", "kind", "first_effect", "type", "number", "second_effect")
@@ -22,26 +36,37 @@ shinyServer(function(input, output) {
     #Joining the data
     final_table <- left_join(drug_names, names_with_id, by = "UMLS_ID") %>% select("drug", "side_effect") 
     final_table <- na.omit(final_table) %>% select(drug, side_effect)
-    if (!is.null(input$drug_name)) {
-      final_table <- final_table %>% filter(drug == input$drug_name) 
-    }
     my_data <- as.data.frame(final_table) 
-    do_not_include <- which(duplicated(my_data))
-    my_data <- my_data[-do_not_include, ] %>% select(side_effect)
-  })
-  output$choices <- renderText({
-    return(my_data)
   })
   output$text <- renderText({
-    print(input$drug_name)
+    print(capitalize(input$drug_name))
   })
   output$table <- DT::renderDataTable(DT::datatable({
-    data <- dataInput()
+    data <- dataInput()    
+    if (!is.null(input$drug_name)) {
+      data <- data %>% filter(drug == input$drug_name) 
+    }
+    do_not_include <- which(duplicated(data))
+    my_data <- data[-do_not_include, ] %>% select(side_effect) 
+    return(my_data)
+  })
+  )
+  output$table2 <- DT::renderDataTable(DT::datatable({
+    data <- dataInput()    
+    if(!is.null(input$effect)){
+      data <- data %>% filter(side_effect == input$effect) 
+    }
+    do_not_include <- which(duplicated(data))
+    my_data <- data[-do_not_include, ] %>% select(drug) 
+    return(my_data)
   })
   )
   output$value <- renderPrint({ input$chosen_effects })
+  output$pie <- renderPlot({
+    data <- dataInput() 
+    data <- data %>% select(side_effect) %>% count(side_effect) %>% arrange(desc(n)) %>% head(10)
+    p <- ggplot(data, aes(x=side_effect, y=n, fill=side_effect))+geom_bar(width = 1, stat = "identity")+coord_polar("y", start=0) +
+      xlab("Side Effect") + ylab("How common side effects are") + title("Pie Chart to Show How Common Side Effects Are")
+    return(p)
+  })
 })
-
-#y <-data.frame(matrix(ncol = 1, nrow = 3))
-#colnames(y) <-  "Tally"
-#row.names(y) <- c("Happy", "Neutral", "Sad")
--- App/ui.R
+++ App/ui.R
@@ -1,48 +1,65 @@
 library(shiny)
 library(ggplot2)
+library(leaflet)
+library(plotly)
+library(Hmisc)
 
 shinyUI(fluidPage(
   fluidPage(
-    titlePanel("Finding Drug/Side Effect Correlation"),
+    titlePanel("Correlation Between Drugs and Side Effects"),
     sidebarLayout(
       sidebarPanel(
-        textInput('drug_name', 'Prescribed Drug Name', value="gamma-aminobutyric")
+        helpText("Write in your prescribed drug or side effect you are currently experiencing!"),
+        textInput('drug_name', 'Prescribed Drug Name', value="gamma-aminobutyric"),
+        textInput('effect', 'What Side Effect Are You Experiencing?', value="Abdominal cramps")
       ),
       mainPanel(
         h1(textOutput("text")),
         tabsetPanel(
-          tabPanel(h4("Overview"),
-                   h2("Purpose"),
-                   "The purpose of our project is to help people track the correlation between their
-                    side effects and the drugs they are prescribed to.",
-                   h2("Audience"),
-                   "Our target audience is people who are taking prescribed medications for a long period of time, 
-                    for example, 6+ months. We want our users to learn from this project is to help them be more 
-                    proactive about their health.",
-                   h2("Data"),
-                   a("The SIDER Side Effect Resource", src = 'http://sideeffects.embl.de/download/'),"was created by a group of scientists who decided to collect public information of side effects for drug target prediction. 
-                    The link is to a report he wrote about the unwanted side effects of drugs. This is partly the inspiration for this project, 
-                    in that we are helping users keep track of possible side effects so they are actively aware and can act to account for these effects.
-                    Citing: Kuhn M, Letunic I, Jensen LJ, Bork P. The SIDER database of drugs and side effects. ",
-                   br(), br(),
-                   a("The Drug.com Database", src = 'https://www.drugs.com/sfx/'), "provides independent information over 24,000 prescription drugs, over-the-counter medicine, and natural products. 
-                   This material is provided for educational purposes only and is not intended for medical advice, diagnosis or treatment. 
-                   Citing: Cerner Multum and Wolters Kluwer.",
-                   h2("Visualizations"),
-                   "For our first plot, when the user gives an input of a drug name, it filters and returns
-                   the side effects associated with the given drug.", br(), br(),
-                   "Our second plot tracks the inverse; when the user gives us side effects they are experiencing, 
-                   we return the drugs which are associated with said side effects.",  br(), br(), 
-                   "Finally, we depict a map of health clinics in Seattle for users to refer to in case they
-                   wish to seek further medical assistance.",
-                   h2("Team Members"),
-                   "Khalid Al Muhairi", br(), "Veronica Chang", br(), "Victoria Huynh", br(), "Zhiwei Zhong"
+          tabPanel(
+            h4("Overview"),
+            h2("Purpose"),
+            "The purpose of our project is to help people track the correlation between their
+            side effects and the drugs they are prescribed to.",
+            h2("Audience"),
+            "Our target audience is people who are taking prescribed medications for a long period of time, 
+            for example, 6+ months. We want our users to learn from this project is to help them be more 
+            proactive about their health.",
+            h2("Data"),
+            a("The SIDER Side Effect Resource", src = 'http://sideeffects.embl.de/download/'),"was created by a group of scientists who decided to collect public information of side effects for drug target prediction. 
+            The link is to a report he wrote about the unwanted side effects of drugs. This is partly the inspiration for this project, 
+            in that we are helping users keep track of possible side effects so they are actively aware and can act to account for these effects.
+            Citing: Kuhn M, Letunic I, Jensen LJ, Bork P. The SIDER database of drugs and side effects.",
+            br(), br(),
+            a("The Drug.com Database", src = 'https://www.drugs.com/sfx/'), "provides independent information over 24,000 prescription drugs, over-the-counter medicine, and natural products. 
+            This material is provided for educational purposes only and is not intended for medical advice, diagnosis or treatment. 
+            Citing: Cerner Multum and Wolters Kluwer.",
+            h2("Visualizations"),
+            "For our first plot, when the user gives an input of a drug name, it filters and returns
+            the side effects associated with the given drug.", br(), br(),
+            "Our second plot tracks the inverse; when the user gives us side effects they are experiencing, 
+            we return the drugs which are associated with said side effects.",  br(), br(), 
+            "Finally, we depict a map of health clinics in Seattle for users to refer to in case they
+            wish to seek further medical assistance.",
+            h2("Team Members"),
+            "Khalid Al Muhairi", br(), "Veronica Chang", br(), "Victoria Huynh", br(), "Zhiwei Zhong"
           ),
           tabPanel(
-            h4("Side Effects"),
-            DT::dataTableOutput("table"))
+            h4("Drug's Side Effects"),
+            DT::dataTableOutput("table")),
+          tabPanel(
+            h4("Possible Drug"),
+            DT::dataTableOutput("table2")),
+          tabPanel(
+            h4("Pie Chart"),
+            plotOutput("pie")
+          ),
+          tabPanel(
+            h4("Map of Seattle Medical Centers"),
+            leafletOutput("map",height = 1000)
+          )
         ))
     )
-  )
+    )
 )
 )
